1c2ce05f3e175bffcade1e11e63df4d6be5fec92
Rafael Moreira <92860308+raffnb@users.noreply.github.com>
2023-12-13 20:53:07 -0300

    Contrução da Tela de Cadastro e seus respectivos componentes

---
+++ frontend/src/app/cadastro/page.jsx
@@ -0,0 +1,14 @@
+import CadastroForm from '@/components/CadastroForm'
+import React from 'react'
+
+const CadastroPage = () => {
+  return (
+    <div>
+
+    <CadastroForm />
+
+    </div>
+  )
+}
+
+export default CadastroPage
